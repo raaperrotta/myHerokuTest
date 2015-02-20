338bcdcca3ce5de08cbf241b9bcbaf116175bab9
raaperrotta <raaperrotta@gmail.com>
2015-02-20 11:54:36 -0500

    Corrected typecast to int in home view

--- hello/views.py
+++ hello/views.py
@@ -9,7 +9,7 @@ def index(request):
     return render(request, 'cover.html')
 
 def home(request, numcols = 18):
-    my_col_range = range(0, numcols as int)
+    my_col_range = range(0, int(numcols))
     return render(request, 'StarterBootstrap.html', {'home_col_text': "Hello, Django!", 'my_col_range': my_col_range})
 
 def carousel(request):
